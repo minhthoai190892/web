08f6c11fd6c74a5f4c00ac9779ef0f2968138b59
ASUS <minhthoai190892@gmail.com>
2023-09-05 01:50:43 +0700

    main

--- Building a Stock Media Platform/js/utils/ripple.js
+++ Building a Stock Media Platform/js/utils/ripple.js
@@ -10,9 +10,31 @@
  */
 export const ripple = function ($rippleElem) {
     console.log($rippleElem)
-    $rippleElem.addEventListener("pointerdown", function(e) {
+    $rippleElem.addEventListener("pointerdown", function (e) {
         e.stopImmediatePropagation();
+        const $ripple = document.createElement("div");
+        $ripple.classList.add("ripple");
+        this.appendChild($ripple);
+        const removeRipple = () => {
+            $ripple.animate({
+                opacity: 0
+            }, { fil: "forwards", duration: 200 });
+            setTimeout(() => {
+                $ripple.remove();
+            },1000)
+        }
+        this.addEventListener("pointerup",removeRipple);
+        this.addEventListener("pointerleave",removeRipple);
+        const /**{Boolean} */ isNotIconButton = !this.classList.contains("icon-btn");
+        if (isNotIconButton) {
+            const /**{Number} */rippleSize = Math.max(this.clientWidth, this.clientHeight);
+            $ripple.style.top=`${e.layerY}px`;
+            $ripple.style.left=`${e.layerY}px`;
+            $ripple.style.width=`${e.layerY}px`;
+            $ripple.style.height=`${e.layerY}px`;
+        }
     });
+
 }
 
 
